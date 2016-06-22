9af6f9ebac7eebc8c56978aed1878d638e8d669b
Mike Miles <MMiles@genuineinteractive.com>
2016-06-21 22:00:34 -0400

    Update to be a plugin. Add highlight functionality

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Overview
+# Reveal.js Code block Line Numbers
+
+## Overview
 
 A 'library' for adding line numbers to code stylized using [highlight.js](http://softwaremaniacs.org/soft/highlight/en/) in a [Reveal.js](https://github.com/hakimel/reveal.js) presentation.
 
@@ -6,28 +8,30 @@ See a [live example](https://mikemiles86.github.io/reveal-line-numbers/#/)
 
 To learn more about this library, [read my blog post](http://www.mike-miles.com/blog/displaying-line-numbers-code-examples-revealjs-presentations) about writing it.
 
-# Dependencies
+## Dependencies
 
 This library depends on using Highlight.js in your Reveal.js presentation.
 
-# File placement
-Relative to the root of your Reveal.js presentation:
+## File placement
 
-* place line-numbers.css into '/lib/css/'
-* place line-numbers.js into '/lib/js/'
+Place these files into 'plugin/line-numbers/';
 
-# Adding library to presentation
+### Adding library to presentation
 To use this library in a Reveal.js presentation, you need to add the 'line-numbers.js' to the list of dependencies in 'Reveal.initialize'.
 
 ```javascript
 Reveal.initialize({
   dependencies: [
-    {src: 'lib/js/line-numbers.js'}
+   // ...
+    {src: 'plugin/line-numbers.js'}
+    // ...
   ]
 })
 ```
 
-# Adding line numbers to code blocks
+## Usage
+
+### Adding line numbers to code blocks
 To add line numbers to a code block, just add the class 'line-numbers' to the <code> element
 
 ```html
@@ -39,6 +43,65 @@ function my_example_code($line_numbers = TRUE) {
   $message = 'This example code does';
   $message .= ($line_numbers ? '' : ' not');
   $message .= ' have line numbers.';
+
+  return $message;
+}
+
+print my_example_code(TRUE);
+</code></pre>
+```
+
+### Highlighting line numbers.
+
+To highlight particular line numbers in a code block, add the attribute 'data-highlight-lines' to the <code> element. Seperate the lines you would like to highlight with a comma(,). Specifcy a group of lines to highlight by seperating the start line and end line with a dash(-).
+
+Example: to highlight the lines 1,2,3:
+```html
+<pre><code class="line-numbers" data-highlight-lines="1,2,3">
+/**
+ * Example Code.
+ */
+function my_example_code($line_numbers = TRUE) {
+  $message = 'This example code does';
+  $message .= 'Will have line numbers';
+  $message .= 'and will have highlighed line numbers.';
+
+  return $message;
+}
+
+print my_example_code(TRUE);
+</code></pre>
+```
+
+Example: To highlight the lines 4 to 10:
+```html
+<pre><code class="line-numbers" data-highlight-lines="4-10">
+/**
+ * Example Code.
+ */
+function my_example_code($line_numbers = TRUE) {
+  $message = 'This example code does';
+  $message .= 'Will have line numbers';
+  $message .= 'and will have highlighed line numbers.';
+
+  return $message;
+}
+
+print my_example_code(TRUE);
+</code></pre>
+```
+
+Example: highlighting lines 1 to 3 and 5,7,9:
+
+```html
+<pre><code class="line-numbers" data-highlight-lines="1-3,5,7,9">
+/**
+ * Example Code.
+ */
+function my_example_code($line_numbers = TRUE) {
+  $message = 'This example code does';
+  $message .= 'Will have line numbers';
+  $message .= 'and will have highlighed line numbers.';
 
   return $message;
 }
--- line-numbers.js
+++ line-numbers.js
@@ -4,25 +4,17 @@
  * Add this javascript library to a reaveal.js presentation by adding it as a
  * dependency in Reveal.initialize.
  *
- * Reveal.initialize({
- *   ....
- *   dependencies: [
- *     ...
- *     {src: 'lib/js/line-numbers.js'}
- *   ]
- * })
- *
  */
 
 // Add an event listener when reveal is finished loading.
 Reveal.addEventListener( 'ready', function( event ) {
  // Check to see if code blocks with line-numbers class exist.
- if ($('code.line-numbers').length > 0) {
+ if (document.getElementsByClassName("line-numbers").length > 0) {
     // Include css.
     var link = document.createElement( 'link' );
     link.rel = 'stylesheet';
     link.type = 'text/css';
-    link.href = 'lib/css/line-numbers.css';
+    link.href = 'plugins/line-numbers/line-numbers.css';
     // Add CSS to head
     document.getElementsByTagName( 'head' )[0].appendChild( link );
   }
@@ -31,21 +23,88 @@ Reveal.addEventListener( 'ready', function( event ) {
 // Adding an event listener on slidechanged event to add line numbers to
 // code blocks.
 Reveal.addEventListener('slidechanged', function(event) {
-  // For any code blocks in the current slide with class 'line-numbers'.
-  $('code.line-numbers').each(function(){
-    // Check ihighlight js has already run and not already added line numbers
-    if ($(this).hasClass('hljs') && ($(this).html().indexOf('class="line-number') < 1)) {
-      // Get the content of the code block.
-      var content = $(this).html();
-      // Split the content on new lines "\n".
-      content = content.split("\n");
-      // Re-join the content with line number spans after the new line.
-      content = content.join("\n<span class='line-number'> </span>");
-      // Prefix with a line number for the first line.
-      content = '<span class="line-number"> </span>' + content;
-      // Replace the content of the code block with the new content.
-      $(this).html(content);
-    }
-  });
+  addLineNumbers();
 });
 
+function addLineNumbers() {
+  // For any code blocks in the current slide with class 'line-numbers'.
+  var line_numbers = document.getElementsByClassName("line-numbers");
+  // Store line numbers to highlight
+  var highlights = [];
+  // Loop through all code blocks
+  for (var l = 0; l < line_numbers.length; l++) {
+    highlights = [];
+    // See if current block has highlight js and line number not already applied.
+    if (line_numbers[l].classList.contains('hljs') && (line_numbers[l].innerHTML.indexOf("line-number") < 1)) {
+      // See if there are any lines that need to be highlighted.
+      if (line_numbers[l].hasAttribute("data-highlight-lines")) {
+        // Get the lines to be highlighted.
+        highlights = getLineNumberHighlights(line_numbers[l].getAttribute("data-highlight-lines"));
+      }
+
+      // Get the content of the code block.
+      var content = line_numbers[l].innerHTML;
+      var classes = '';
+      // Split the code block at new line.
+      content = content.split("\n");
+      // Loop through each line.
+      for (var n = 0; n < content.length; n++) {
+        // Add class 'line-number'.
+        classes = 'line-number';
+        // Should this line be highlighted?
+        if (highlights.indexOf(n+1) > -1) {
+          classes += ' highlight-line';
+        }
+        // Append line with line number span.
+        content[n] = "<span class=\"" + classes + "\"> </span>" + content[n];
+      }
+      // Rejoin all content.
+      content = content.join("\n");
+      // Replace content of code block.
+      line_numbers[l].innerHTML = content;
+    }
+  }
+}
+
+// Determins which lines from a range to be highlighted.
+function getLineNumberHighlights(line_numbers) {
+  var highlights = num_range = [];
+  var start = end = num = 0;
+  // Split line groups on comma
+  if (line_numbers.indexOf(',') > 0) {
+    line_numbers = line_numbers.split(",");
+  }
+  else {
+    // No seperator, assume it is just a single line.
+    line_numbers = [line_numbers];
+  }
+
+  // Loop through each number group.
+  for(n = 0; n < line_numbers.length; n++) {
+    // Is number group a range?
+    if (line_numbers[n].indexOf("-") > 0) {
+      // split on seperator
+      num_range = line_numbers[n].split("-");
+      start = Number.parseInt(num_range[0]);
+      end = Number.parseInt(num_range[1]);
+      // Add all lines between ranges
+      while (start <= end) {
+        // Check if line number is not already in list.
+        if (highlights.indexOf(start) < 0) {
+          highlights.push(start);
+        }
+        start++;
+      }
+    }
+    else {
+      // Else assume a single line number
+      num = Number.parseInt(line_numbers[n]);
+      // Confirm that line number is not already in array.
+      if ((num == line_numbers[n]) && highlights.indexOf(num) < 0) {
+        highlights.push(num);
+      }
+    }
+  }
+  // Return count of hightlight lines.
+  return highlights;
+}
